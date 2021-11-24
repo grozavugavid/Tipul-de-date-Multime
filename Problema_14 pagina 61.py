0f3bea4640290d303013a40ec2b8b6aca7f468f7
grozavudavid <70814477+grozavugavid@users.noreply.github.com>
2021-11-24 20:40:23 +0200

    Add files via upload

---
+++ Problema_14 pagina 61.py
@@ -0,0 +1,4 @@
+s1={'I want to break free'}
+s2={'Who let the dogs out'}
+print('Caracterele care apar in ambele siruri sunt',s1.intersection(s2))
+print('Caracterele care apar in primul sir si nu apare in al doilea sunt',s1.difference(s2))
